74e16d58c88735b7c607d5b52eb60f77e416ede3
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-10-24 18:19:56 -0300

    refactor: alteração no `ApiClient` p/ uso do 'HttpClient'

--- src/main/java/com/currencyconverter/ApiClient.java
+++ src/main/java/com/currencyconverter/ApiClient.java
@@ -1,36 +1,47 @@
 package com.currencyconverter;
 
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiClient {
-    private static final String API_KEY = Config.get("api.key");
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/";
+    private static final String API_KEY = Config.get("api.key");
+
+    private static final HttpClient client = HttpClient.newHttpClient();
 
     public static double getExchangeRate(String fromCurrency, String toCurrency) throws Exception {
         if (API_KEY == null || API_KEY.isEmpty()) {
             throw new IllegalStateException("( ! ) :: A 'API_KEY' não foi configurada\n");
         }
 
-        URL url = new URL(BASE_URL + API_KEY + "/latest/" + fromCurrency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+        String url = BASE_URL + API_KEY + "/latest/" + fromCurrency;
 
-        if (connection.getResponseCode() != 200) {
-            throw new RuntimeException("Erro na API: " + connection.getResponseCode());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Erro ao conectar-se à API: " + e.getMessage(), e);
         }
 
-        InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-        JsonObject jsonResponse = JsonParser.parseReader(reader).getAsJsonObject();
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Erro na API: HTTP " + response.statusCode());
+        }
 
-        reader.close();
-        connection.disconnect();
-
+        JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
         JsonObject conversionRates = jsonResponse.getAsJsonObject("conversion_rates");
+
         return conversionRates.get(toCurrency).getAsDouble();
     }
 }
