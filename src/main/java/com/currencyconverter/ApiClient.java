b53847726e287ee7734f4277575198eb4a647123
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-10-28 15:28:33 -0300

    refactor: incluída `PUBLIC_KEY` p/ uso

--- src/main/java/com/currencyconverter/ApiClient.java
+++ src/main/java/com/currencyconverter/ApiClient.java
@@ -8,19 +8,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
 
 public class ApiClient {
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/";
     private static final String API_KEY = Config.get("api.key");
+    private static final String PUBLIC_KEY = Config.get("api.public-key");
 
     private static final HttpClient client = HttpClient.newHttpClient();
 
     public static double getExchangeRate(String fromCurrency, String toCurrency) throws Exception {
-        if (API_KEY == null || API_KEY.isEmpty()) {
-            throw new IllegalStateException("( ! ) :: A 'API_KEY' não foi configurada\n");
-        }
 
-        String url = BASE_URL + API_KEY + "/latest/" + fromCurrency;
+        String url;
+
+        if (!Objects.equals(API_KEY, "SUA_KEY_AQUI")) {
+            url = BASE_URL + API_KEY + "/latest/" + fromCurrency;
+
+        } else if (PUBLIC_KEY != null) {
+            url = BASE_URL + PUBLIC_KEY + "/latest/" + fromCurrency;
+
+        } else {
+            throw new IllegalStateException("( ! ) :: Nenhuma das chaves de acesso foi encontrada ou configurada\n");
+        }
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -32,7 +41,7 @@ public class ApiClient {
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Erro ao conectar-se à API: " + e.getMessage(), e);
+            throw new RuntimeException("Erro ao se conectar à API: " + e.getMessage(), e);
         }
 
         if (response.statusCode() != 200) {
